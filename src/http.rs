36bdc812a4f4d4c163473a6197f5bf23b4573641
Ludea <ludovicw35@hotmail.com>
2024-10-30 12:36:58 +0100

    Update http.rs

--- src/http.rs
+++ src/http.rs
@@ -13,7 +13,7 @@ async fn health_check() -> &'static str {
 }
 
 pub async fn serve_dir() {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     let local_addr = listener.local_addr().unwrap();
     tracing::info!("HTTP listening on {local_addr}");
