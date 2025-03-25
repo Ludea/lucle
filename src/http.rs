6bb3cfad0e4895e1703453f79053744bf5f582fd
Ludea <ludealol.olol@gmail.com>
2025-03-25 09:51:34 +0100

    wop

--- src/http.rs
+++ src/http.rs
@@ -1,30 +1,18 @@
 use axum::{
-    handler::HandlerWithoutStateExt,
-    http::StatusCode,
     routing::{get, get_service},
     Router,
 };
-use tower_http::{
-    services::{ServeDir, ServeFile},
-    trace::TraceLayer,
-};
+use tower_http::{services::ServeDir, trace::TraceLayer};
 
 async fn health_check() -> &'static str {
     "OK"
 }
 
 pub fn serve_dir() -> Router {
-    async fn handle_404() -> (StatusCode, &'static str) {
-        (StatusCode::NOT_FOUND, "Not found")
-    }
-
-    let service = handle_404.into_service();
-    let serve_dir = ServeDir::new("web/dist")
-        .fallback(ServeFile::new("web/dist/index.html"))
-        .not_found_service(service);
+    let serve_dir = ServeDir::new("web/dist");
 
     Router::new()
         .route("/health", get(health_check))
-        .route("/", get_service(serve_dir))
+        .route_service("/", get_service(serve_dir))
         .layer(TraceLayer::new_for_http())
 }
--- src/main.rs
+++ src/main.rs
@@ -97,16 +97,12 @@ async fn main() {
         .with_single_cert(certs, private_key)
         .unwrap();
 
-    /*     if let Err(err) = tokio::join!(http::serve_dir()) {
-           tracing::error!("GRPC and HTTP server doesn't start: {err}");
-       }
-    */
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
 
-    let grpc = rpc::rpc_api(&mut cert_buf, &mut key_buf, database);
+//    let grpc = rpc::rpc_api(&mut cert_buf, &mut key_buf, database);
     let http = http::serve_dir();
-    let app = Router::new().merge(grpc).merge(http);
+    let app = http;//Router::new().merge(grpc).merge(http);
 
     tracing::info!("gRPC and http server listening on {addr}");
     axum::serve(listener, app).await.unwrap();
