16d13cdca5c0d904d137f55ea6fbf59be7051a32
Ludea <kudealol.olol@gmail.com>
2026-07-21 21:55:06 +0200

    fix: push missing rpc file for launcher

---
+++ web/src/utils/sparusrpc.ts
@@ -0,0 +1,23 @@
+export const send_event_all = async (client: any, event: number, plugin_name?: string) =>
+  client.send_event_all({
+    event_type: event,
+    plugin: plugin_name,
+  });
+
+export const build_custom_launcher = async (
+  client: any,
+  launcher_name: string,
+  repository_name: string,
+  game_name: string,
+  update_server_url: string,
+  plugins_url: string,
+  config_file: string,
+) =>
+  client.create_workflow({
+    launcher_name: launcher_name,
+    repository_name: repository_name,
+    game_name: game_name,
+    speedupdate_server_url: update_server_url,
+    plugins_url: plugins_url,
+    config_file: config_file,
+  });
