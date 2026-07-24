bfb1ed39bca8ad7b88b1ff3e2a0952efa4289030
Ludea <ludealol.olol@gmail.com>
2026-07-24 07:46:02 +0200

    feat: show on / off password

--- web/src/components/Signin.tsx
+++ web/src/components/Signin.tsx
@@ -2,7 +2,14 @@ import { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
 import Checkbox from "@mui/material/Checkbox";
+import InputAdornment from "@mui/material/InputAdornment";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import IconButton from "@mui/material/IconButton";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import { Link } from "react-router-dom";
@@ -11,6 +18,7 @@ function Signin({ onSignin, error }: { onSignin: void; error: string }) {
   const [username, setUsername] = useState<string>(localStorage.getItem("username") || "");
   const [password, setPassword] = useState<string>(localStorage.getItem("password") || "");
   const [remember, setRemember] = useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
   const [emptyUsername, setEmptyUsername] = useState<boolean>(false);
   const [emptyPassword, setEmptyPassword] = useState<boolean>(false);
 
@@ -43,21 +51,35 @@ function Signin({ onSignin, error }: { onSignin: void; error: string }) {
             setUsername(event.target.value);
           }}
         />
-        <TextField
-          error={emptyPassword}
-          autoFocus={emptyPassword}
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          value={password}
-          onChange={(event) => {
-            setPassword(event.target.value);
-          }}
-        />
+        <FormControl variant="outlined" fullWidth required>
+          <InputLabel htmlFor="password">Password</InputLabel>
+          <OutlinedInput
+            id="password"
+            required
+            fullWidth
+            value={password}
+            error={emptyPassword}
+            autoFocus={emptyPassword}
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+            type={showPassword ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label={showPassword ? "hide the password" : "display the password"}
+                  onClick={() => setShowPassword(!showPassword)}
+                  onMouseDown={(event) => event.preventDefault()}
+                  onMouseUp={(event) => event.preventDefault()}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
         <FormControlLabel
           control={
             <Checkbox
