d764ad759f0eee6ace065c6aaee366babd893b8d
Ludea <kudealol.olol@gmail.com>
2026-07-31 19:25:25 +0200

    fix: remove older theme provider

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,11 @@
 import { useState, useEffect, useContext } from "react";
 
-// react-router components
 import { useRoutes } from "react-router-dom";
 
 // RPC Components
 import { checkIfInstalled } from "utils/rpc";
 
-// Material Dashboard 2 React routes
 import routes from "routes";
-
-// @mui material components
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-
-// Material Dashboard 2 React themes
-import theme from "assets/theme";
-
-// Material Dashboard 2 React Dark Mode themes
-import themeDark from "assets/theme-dark";
-
-// Material Dashboard 2 React contexts
-import { useMaterialUIController } from "context";
 
 // Context
 import AuthProvider from "context/Auth";
@@ -29,8 +14,6 @@ import { LucleRPC } from "context/Luclerpc";
 export default function App() {
   const [isInstalled, setIsInstalled] = useState<boolean>();
   const client = useContext(LucleRPC);
-  const [controller] = useMaterialUIController();
-  const { darkMode } = controller;
 
   useEffect(() => {
     checkIfInstalled(client)
@@ -43,12 +26,9 @@ export default function App() {
   }, []);
 
   return (
-    <ThemeProvider theme={darkMode ? themeDark : theme}>
-      <CssBaseline />
       <AuthProvider>
         {isInstalled !== undefined ? <LucleRoutes isInstalled={isInstalled} /> : null}
       </AuthProvider>
-    </ThemeProvider>
   );
 }
 
