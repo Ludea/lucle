15709c29317d9601aa6e2543fe976dddd1f2abdb
Ludea <kudealol.olol@gmail.com>
2026-09-06 21:04:56 +0200

    fix: improve db type

--- web/src/components/DatabaseInfo.tsx
+++ web/src/components/DatabaseInfo.tsx
@@ -2,65 +2,68 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
-interface dbInfos {
-  dbName: string;
-  hostname: string;
-  port: number;
-  username: string;
-  password: string;
-}
+import type { DBInfos } from "layouts/Install";
 
-function DatabaseInfo({ dbInfos, setDBInfos }: { dbInfos: dbInfos; setDBInfos: any }) {
+function DatabaseInfo({
+  dbInfos,
+  setDBInfos,
+}: {
+  dbInfos: DBInfos | undefined;
+  setDBInfos: (infos: DBInfos) => void;
+}) {
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { id, value } = event.target;
     setDBInfos({
       ...dbInfos,
-      [id]: value,
+      [id]: id === "port" ? Number(value) : value,
     });
   };
 
   return (
-    <Box
-      sx={{
-        marginTop: 8,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <Box sx={{ mt: 1 }}>
-        <TextField margin="normal" id="dbName" label="Database name" onChange={handleChange} />
-        <TextField
-          required
-          margin="normal"
-          id="hostname"
-          label="Database hostname"
-          onChange={handleChange}
-        />
-        <TextField
-          required
-          label="Port"
-          type="number"
-          margin="normal"
-          id="port"
-          onChange={handleChange}
-        />
-        <TextField
-          required
-          margin="normal"
-          label="Username"
-          id="username"
-          onChange={handleChange}
-        />
-        <TextField
-          required
-          label="Password"
-          id="password"
-          margin="normal"
-          type="password"
-          onChange={handleChange}
-        />
-      </Box>
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+      <TextField
+        fullWidth
+        id="dbName"
+        label="Database name"
+        slotProps={{ htmlInput: { style: { fontSize: 16 } } }}
+        onChange={handleChange}
+      />
+      <TextField
+        fullWidth
+        required
+        id="hostname"
+        label="Hostname"
+        slotProps={{ htmlInput: { style: { fontSize: 16 } } }}
+        onChange={handleChange}
+      />
+      <TextField
+        fullWidth
+        required
+        id="port"
+        label="Port"
+        type="number"
+        slotProps={{ htmlInput: { style: { fontSize: 16 }, min: 1, max: 65535 } }}
+        onChange={handleChange}
+      />
+      <TextField
+        fullWidth
+        required
+        id="username"
+        label="Username"
+        autoComplete="username"
+        slotProps={{ htmlInput: { style: { fontSize: 16 } } }}
+        onChange={handleChange}
+      />
+      <TextField
+        fullWidth
+        required
+        id="password"
+        label="Password"
+        type="password"
+        autoComplete="new-password"
+        slotProps={{ htmlInput: { style: { fontSize: 16 } } }}
+        onChange={handleChange}
+      />
     </Box>
   );
 }
